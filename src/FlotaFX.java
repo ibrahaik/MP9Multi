31ba38eaeb061050a205e45f1f589e484daa4702
haiko <ibrahim.haik.el.haouzi.mrabet@elpuig.xeill.net>
2025-03-03 14:07:50 +0100

    VERSION FINAL

---
+++ src/FlotaFX.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class FlotaFX extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
